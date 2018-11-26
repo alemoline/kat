1191889ae524b8d76d83ad84fa84c8b5022010bf
Alejandro <alemoline@gmail.com>
2018-11-26 14:52:11 -0300

    Added a check: exits if no command-line arguments are given.

--- kat.py
+++ kat.py
@@ -5,6 +5,12 @@
 
 import sys
 
-print "This is the name of the script: ", sys.argv[0]
-print "Number of arguments: ", len(sys.argv)
-print "The arguments are: " , str(sys.argv)
+# print "This is the name of the script: ", sys.argv[0]
+# print "Number of arguments: ", len(sys.argv)
+# print "The arguments are: " , str(sys.argv)
+
+if len(sys.argv):
+    print "kat: please specify a file name." # exits if no command-line arguments are given
+    quit()
+
+print "So far, so good."
